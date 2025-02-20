b2337925297f4e0275618b6b06bd1c92289f95cc
hjmcphail76 <hjmcphail76@gmail.com>
2025-02-20 16:40:27 -0500

    small changes nothing big

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,7 +3,8 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-import edu.wpi.first.epilogue.Epilogue;
+
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.wpilibj.DataLogManager;
@@ -13,7 +14,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.automation.AutomatedScoring;
 import frc.robot.automation.AutomationSelector;
-
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -33,10 +33,8 @@ public class Robot extends TimedRobot {
 
     public Robot() {
         DataLogManager.start();
-        Epilogue.bind(this);
     }
 
-   
     /**
      * This function is called every 20 ms, no matter the mode. Use this for items
      * like diagnostics
