5dbcca63b6832abe70bc7a327306be06374ef716
hjmcphail76 <hjmcphail76@gmail.com>
2025-03-15 21:45:51 -0400

    Individual poses automated score added with 2d array

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -22,77 +22,75 @@ public final class RobotConstants {
 
         public static final class ScoringConstants {
                 public static final class BlueAlliance {
-                        // Pose2d[][] REEF_SIDE_POSES = new Pose2d[6][3];
-                        
-                        // // Reef 1 LEFT
-                        // REEF_SIDE_POSES[0][0] = new Pose2d(3.146, 4.191, new Rotation2d(Math.toRadians(0)));
 
-                        // // Reef 1 ALGAE
-                        // REEF_SIDE_POSES[0][1] = new Pose2d(3.146, 4.025, new Rotation2d(Math.toRadians(0)));
+                        public static final Pose2d[][] REEF_SIDE_POSES;
 
-                        // // TODO Reef 1 RIGHT
-                        // REEF_SIDE_POSES[0][2] = new Pose2d(3.146, 3.859, new Rotation2d(Math.toRadians(0)));
+                        static {
+                                REEF_SIDE_POSES = new Pose2d[6][3];
 
+                                // Reef 1 LEFT
+                                REEF_SIDE_POSES[0][0] = new Pose2d(3.146, 4.191, new Rotation2d(Math.toRadians(0)));
 
-                        // // Reef 2 LEFT
-                        // REEF_SIDE_POSES[1][0] = new Pose2d(3.659, 2.947, new Rotation2d(Math.toRadians(60)));
+                                // Reef 1 ALGAE
+                                REEF_SIDE_POSES[0][1] = new Pose2d(3.146, 4.025, new Rotation2d(Math.toRadians(0)));
 
-                        // // Reef 2 ALGAE
-                        // REEF_SIDE_POSES[1][1] = new Pose2d(3.812, 2.861, new Rotation2d(Math.toRadians(60)));
+                                // TODO Reef 1 RIGHT
+                                REEF_SIDE_POSES[0][2] = new Pose2d(3.146, 3.859, new Rotation2d(Math.toRadians(0)));
 
-                        // // TODO Reef 2 RIGHT
-                        // REEF_SIDE_POSES[1][2] = new Pose2d(3.978, 2.801, new Rotation2d(Math.toRadians(60)));
+                                // Reef 2 LEFT
+                                REEF_SIDE_POSES[1][0] = new Pose2d(3.659, 2.947, new Rotation2d(Math.toRadians(60)));
 
+                                // Reef 2 ALGAE
+                                REEF_SIDE_POSES[1][1] = new Pose2d(3.812, 2.861, new Rotation2d(Math.toRadians(60)));
 
-                        // // Reef 3 LEFT
-                        // REEF_SIDE_POSES[2][0] = new Pose2d(5.009, 2.781, new Rotation2d(Math.toRadians(120)));
+                                // TODO Reef 2 RIGHT
+                                REEF_SIDE_POSES[1][2] = new Pose2d(3.978, 2.801, new Rotation2d(Math.toRadians(60)));
 
-                        // // Reef 3 ALGAE
-                        // REEF_SIDE_POSES[2][1] = new Pose2d(5.168, 2.868, new Rotation2d(Math.toRadians(120)));
+                                // Reef 3 LEFT
+                                REEF_SIDE_POSES[2][0] = new Pose2d(5.009, 2.781, new Rotation2d(Math.toRadians(120)));
 
-                        // // TODO Reef 3 RIGHT
-                        // REEF_SIDE_POSES[2][2] = new Pose2d(5.302, 2.954, new Rotation2d(Math.toRadians(120)));
+                                // Reef 3 ALGAE
+                                REEF_SIDE_POSES[2][1] = new Pose2d(5.168, 2.868, new Rotation2d(Math.toRadians(120)));
 
+                                // TODO Reef 3 RIGHT
+                                REEF_SIDE_POSES[2][2] = new Pose2d(5.302, 2.954, new Rotation2d(Math.toRadians(120)));
 
-                        // // Reef 4 LEFT
-                        // REEF_SIDE_POSES[3][0] = new Pose2d(5.827, 3.839, new Rotation2d(Math.toRadians(180)));
+                                // Reef 4 LEFT
+                                REEF_SIDE_POSES[3][0] = new Pose2d(5.827, 3.839, new Rotation2d(Math.toRadians(180)));
 
-                        // // Reef 4 ALGAE
-                        // REEF_SIDE_POSES[3][1] = new Pose2d(5.827, 4.025, new Rotation2d(Math.toRadians(180)));
+                                // Reef 4 ALGAE
+                                REEF_SIDE_POSES[3][1] = new Pose2d(5.827, 4.025, new Rotation2d(Math.toRadians(180)));
 
-                        // // TODO Reef 4 RIGHT
-                        // REEF_SIDE_POSES[3][2] = new Pose2d(5.827, 4.171, new Rotation2d(Math.toRadians(180)));
+                                // TODO Reef 4 RIGHT
+                                REEF_SIDE_POSES[3][2] = new Pose2d(5.827, 4.171, new Rotation2d(Math.toRadians(180)));
 
+                                // Reef 5 LEFT
+                                REEF_SIDE_POSES[4][0] = new Pose2d(5.308, 5.103, new Rotation2d(Math.toRadians(-120)));
 
-                        // // Reef 5 LEFT
-                        // REEF_SIDE_POSES[4][0] = new Pose2d(5.308, 5.103, new Rotation2d(Math.toRadians(-120)));
+                                // Reef 5 ALGAE
+                                REEF_SIDE_POSES[4][1] = new Pose2d(5.155, 5.183, new Rotation2d(Math.toRadians(-120)));
 
-                        // // Reef 5 ALGAE
-                        // REEF_SIDE_POSES[4][1] = new Pose2d(5.155, 5.183, new Rotation2d(Math.toRadians(-120)));
+                                // TODO Reef 5 RIGHT
+                                REEF_SIDE_POSES[4][2] = new Pose2d(5.022, 5.262, new Rotation2d(Math.toRadians(-120)));
 
-                        // // TODO Reef 5 RIGHT
-                        // REEF_SIDE_POSES[4][2] = new Pose2d(5.022, 5.262, new Rotation2d(Math.toRadians(-120)));
+                                // Reef 6 LEFT
+                                REEF_SIDE_POSES[5][0] = new Pose2d(3.965, 5.269, new Rotation2d(Math.toRadians(-60)));
 
-                        
-                        // // Reef 6 LEFT
-                        // REEF_SIDE_POSES[5][0] = new Pose2d(3.965, 5.269, new Rotation2d(Math.toRadians(-60)));
+                                // Reef 6 ALGAE
+                                REEF_SIDE_POSES[5][1] = new Pose2d(3.825, 5.183, new Rotation2d(Math.toRadians(-60)));
 
-                        // // Reef 6 ALGAE
-                        // REEF_SIDE_POSES[5][1] = new Pose2d(3.825, 5.183, new Rotation2d(Math.toRadians(-60)));
+                                // TODO Reef 6 RIGHT
+                                REEF_SIDE_POSES[5][2] = new Pose2d(3.665, 5.096, new Rotation2d(Math.toRadians(-60)));
+                        }
 
-                        // // TODO Reef 6 RIGHT
-                        // REEF_SIDE_POSES[5][2] = new Pose2d(3.665, 5.096, new Rotation2d(Math.toRadians(-60)));
-
-
-                        //OLD LIST BEFORE WE CHANGED TO MULTI DIMENSIONAL ARRAY FOR REEF POSES
-                        public static final List<Pose2d> REEF_SIDE_POSES = List.of(
-                                        new Pose2d(3.153, 4.025, new Rotation2d(Math.toRadians(0))),
-                                        new Pose2d(3.822, 2.874, new Rotation2d(Math.toRadians(60))),
-                                        new Pose2d(5.168, 2.904, new Rotation2d(Math.toRadians(120))),
-                                        new Pose2d(5.766, 4.001, new Rotation2d(Math.toRadians(180))),
-                                        new Pose2d(5.203, 5.152, new Rotation2d(Math.toRadians(-120))),
-                                        new Pose2d(3.800, 5.164, new Rotation2d(Math.toRadians(-60))));
-                        
+                        // OLD LIST BEFORE WE CHANGED TO MULTI DIMENSIONAL ARRAY FOR REEF POSES
+                        // public static final List<Pose2d> REEF_SIDE_POSES = List.of(
+                        // new Pose2d(3.153, 4.025, new Rotation2d(Math.toRadians(0))),
+                        // new Pose2d(3.822, 2.874, new Rotation2d(Math.toRadians(60))),
+                        // new Pose2d(5.168, 2.904, new Rotation2d(Math.toRadians(120))),
+                        // new Pose2d(5.766, 4.001, new Rotation2d(Math.toRadians(180))),
+                        // new Pose2d(5.203, 5.152, new Rotation2d(Math.toRadians(-120))),
+                        // new Pose2d(3.800, 5.164, new Rotation2d(Math.toRadians(-60))));
 
                         public static final List<Pose2d> HP_POSES = List.of(
                                         new Pose2d(1.11, 7.125, new Rotation2d(Units.degreesToRadians(125))),
@@ -125,9 +123,9 @@ public final class RobotConstants {
 
                 // Chassis configuration
 
-                public static final double DRIVE_BASE_RADIUS_METERS = Units.inchesToMeters(17.5);; // measurement from
-                                                                                                   // center point of
-                                                                                                   // robot
+                public static final double DRIVE_BASE_RADIUS_METERS = Units.inchesToMeters(17.5); // measurement from
+                                                                                                  // center point of
+                                                                                                  // robot
                 // to the
                 // center of one of the wheels. (use the
                 // CAD)
